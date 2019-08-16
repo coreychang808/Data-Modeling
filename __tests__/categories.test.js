5160189f755282abc104b1f688463ea0bdadf960
coreychang808 <coreychang808@hotmail.com>
2019-08-15 22:40:07 -0700

    start

---
+++ __tests__/categories.test.js
@@ -0,0 +1,65 @@
+const Categories = require('../categories/categories.js');
+
+describe('Categories Model', () => {
+
+  let categories;
+
+  beforeEach(() => {
+    categories = new Categories();
+  })
+
+  // How might we repeat this to check on types?
+  it('sanitize() returns undefined with missing requirements', () => {
+    const schema = categories.schema;
+    var testRecord = {};
+    for (var field in schema) {
+      if (schema[field].required) {
+        testRecord[field] = null;
+      }
+    }
+    expect(categories.sanitize(testRecord)).toBeUndefined();
+  });
+
+  it('can post() a new category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        Object.keys(obj).forEach(key => {
+          expect(record[key]).toEqual(obj[key]);
+        });
+      })
+      .catch(e => console.error('ERR', e));
+  });
+
+  it('can get() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.get(record._id)
+          .then(category => {
+            Object.keys(obj).forEach(key => {
+              expect(category[0][key]).toEqual(obj[key]);
+            });
+          });
+      });
+  });
+
+  it('can update() a category', () => {
+    let obj = { name: 'Test Category' };
+    return categories.create(obj)
+      .then(record => {
+        return categories.update(record._id,obj)
+          .then (record => {
+            expect(record.name).toEqual(obj.name);
+          })
+          });
+        });
+  
+  it('can delete() a category', () => {
+    let obj = { name: 'Test Category'};
+    return categories.delete(obj)
+      .then(record =>{
+        expect(record).toBeUndefined();
+      })
+  })
+});
